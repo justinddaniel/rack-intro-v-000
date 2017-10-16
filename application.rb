88a2b32e9850ac9401099f01565946ce47a51187
Justin Daniel <justinddaniel@gmail.com>
2017-10-16 20:10:00 +0000

    automatically backed up by learn

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World. Why is 'hello, world?' the standard programmer boilerplate? I guess it is better than 'fuck off'!"
+    resp.write "Hello, World. Why is 'hello, world' the standard programmer boilerplate? I guess it is better than 'fuck off'!"
     resp.finish
   end
 
